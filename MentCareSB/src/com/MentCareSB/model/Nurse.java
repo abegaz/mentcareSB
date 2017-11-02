ec5c21d97311d6135e2641986f4d80f735cd21c7
jacobyprogramming <jacob.malimban@gmail.com>
2017-11-02 16:01:18 -0400

    Updated Nurse.java

--- MentCareSB/src/com/MentCareSB/model/Nurse.java
+++ MentCareSB/src/com/MentCareSB/model/Nurse.java
@@ -2,5 +2,33 @@ package com.MentCareSB.model;
 
 public class Nurse 
 {
-
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String task;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getTask() {
+		return task;
+	}
+	public void setTask(String task) {
+		this.task = task;
+	}
 }
